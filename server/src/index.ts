1511c9d8962ee45d688deb96976c623736e9a236
Sargun Singh Sethi <100695155+SargunSinghSethi@users.noreply.github.com>
2024-08-21 17:50:07 +0000

    Dashboard Backend API Route Completed

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,3 +5,26 @@ import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
 // ROUTE IMPORTS
+import dashboardRoutes from "./routes/dashboardRoutes";
+
+// CONFIGURATIONS
+
+dontenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+
+/* ROUTES */
+
+app.use("/dashboard",dashboardRoutes);
+
+/* Server */
+const port = process.env.PORT || 3001;
+app.listen(port, () => {
+    console.log(`Serverd running at port ${port}`);
+})
