6eb6d72099b0f8abd1c8ec72abb4e3e68e431b0a
Sargun Singh Sethi <100695155+SargunSinghSethi@users.noreply.github.com>
2024-08-24 18:51:07 +0530

    Update AWS S3

--- client/src/app/(components)/Navbar/index.tsx
+++ client/src/app/(components)/Navbar/index.tsx
@@ -1,7 +1,8 @@
 import { useAppDispatch, useAppSelector } from "@/app/redux";
 import { setIsDarkMode, setIsSidebarCollapsed } from "@/state";
-import { Bell, Menu, Moon, Settings, Sun } from "lucide-react"
-import Link from "next/link"
+import { Bell, Menu, Moon, Settings, Sun } from "lucide-react";
+import Link from "next/link";
+import Image from "next/image";
 
 const Navbar = () => {
   const dispatch = useAppDispatch();
@@ -52,9 +53,13 @@ const Navbar = () => {
           </div>
           <hr className="w-0 h-7 border border-solid border-l border-gray-300 mx-3" />
           <div className="flex items-center gap-3 cursor-pointer">
-            <div className="w-9 h-9">
-              image
-            </div>
+            <Image
+              src="https://s-inventory.s3.ap-south-1.amazonaws.com/profile.jpg"
+              alt="Profile"
+              width={50}
+              height={50}
+              className="rounded-full h-full object-cover"
+            />
             <span className="font-semibold">
               Sargun
             </span>
