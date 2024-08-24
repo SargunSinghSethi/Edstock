c9c0aabd8da20da8685a1ebac2302c78bbd23bdb
Sargun Singh Sethi <100695155+SargunSinghSethi@users.noreply.github.com>
2024-08-24 18:54:06 +0530

    Update AWS S3

--- client/src/app/(components)/Sidebar/index.tsx
+++ client/src/app/(components)/Sidebar/index.tsx
@@ -4,6 +4,7 @@ import { setIsSidebarCollapsed } from "@/state";
 import { Archive, CircleDollarSign, Clipboard, Layout, LucideIcon, Menu, Settings, SlidersHorizontal, User } from "lucide-react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import Image from "next/image";
 
 interface SidebarLinkProps {
   href: string;
@@ -51,7 +52,13 @@ const Sidebar = () => {
     <div className={sidebarClassNames}>
       {/* TOP LOGO */}
       <div className={`flex gap-3 justify-between md:justify-normal items-center pt-8 ${isSidebarCollapsed ? "px-5" : "px-8"}`}>
-        <div>logo</div>
+        <Image
+          src="https://s-inventory.s3.ap-south-1.amazonaws.com/logo.png"
+          alt="edstock-logo"
+          width={27}
+          height={27}
+          className="rounded w-8"
+        />
         <h1 className={`font-extrabold text-2xl ${isSidebarCollapsed ? "hidden" : "blockx`"}`}>EDSTOCK</h1>
         <button className="md:hidden px-3 py-3 bg-gray-100 rounded-full hover:bg-blue-100" onClick={toggleSidebar}>
           <Menu className="w-4 h-4 " />
