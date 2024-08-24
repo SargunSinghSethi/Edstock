3d4a937a51d8f8a3f5c743136e0b9993b5636f56
Sargun Singh Sethi <100695155+SargunSinghSethi@users.noreply.github.com>
2024-08-24 19:18:18 +0530

    Update AWS S3

--- client/src/app/dashboard/CardPopularProducts.tsx
+++ client/src/app/dashboard/CardPopularProducts.tsx
@@ -25,7 +25,7 @@ const CardPopularProducts = () => {
               >
                 <div className="flex items-center gap-3">
                   <Image
-                    src={`https://https://s-inventory.s3.ap-south-1.amazonaws.com/product${
+                    src={`https://s-inventory.s3.ap-south-1.amazonaws.com/product${
                       Math.floor(Math.random() * 3) + 1
                     }.png`}
                     alt={product.name}
